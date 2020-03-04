d6b188d7a502930cf1bd4e2a6d8ec5281ce354e5
Aditya Shelar <I523349@WDFN34719926A>
2020-03-04 23:41:51 +0100

    navbar development

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch,Route} from 'react-router-dom';
+import logo from './logoo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
 function App() {
   return (
-    <div className="App">
-      <h1> Fashion </h1>
-      <h2> Fashion </h2>
-      <h3> Fashion App </h3>
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={ProductList} />
+        <Route path='/details' component={Details} />
+        <Route path='/cart' component={Cart} />
+        <Route component={Default} />
+      </Switch>
+    </React.Fragment>
+    
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logoo.png'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+
+            <Link to='/'>
+                <img src={logo} alt="store"
+                className="navbar-brand" height='60'  />
+            </Link>
+
+            <ul className="navbar-nav align-items-center">
+                <li className="nav-item ml-5">
+                    <Link to='/' className="nav-link">
+                        Products
+                    </Link>
+                </li>
+            </ul>
+
+            <Link to='/cart' className="ml-auto">
+                <button>
+                    <i className="fas fa-cart-plus" />
+                    My Cart
+                </button>
+            </Link>
+            </nav>
+        )
+    }
+}
