bb9efb755fdc84acafc6f4d3de97619ba1bedc41
Aditya Shelar <I523349@WDFN34719926A>
2020-03-11 18:55:09 +0100

    Cart Page Design with Add, Remove, Clear functionalities

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,9 +1,53 @@
 import React from 'react'
 
-export default function CartItem({}) {
+export default function CartItem({item,value}) {
+
+    const{id,title,img,price,total,count} = item;
+    const{increment, decrement, removeItem} = value;
     return (
-        <div>
-            This is a cart Item
+        <div className="row my-5 border border-light text-capitalize text-center">
+        <div className="col-10 mx-auto col-lg-2">
+            <img src = {img} style={{width:"6rem", height:"8rem"}} className="img-fluid" alt="product" />
+        </div>
+        
+        <div className="col-10 mx-auto col-lg-2">
+            <span className="d-lg-none">product : </span>
+            {title}
+        </div>
+
+        <div className="col-10 mx-auto col-lg-2">
+            <span className="d-lg-none">price : </span>
+            {price}
+        </div>
+
+        <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+            <div className="d-flex justify-content-center">
+                <div>
+                    <span className="btn btn-cyan mx-1" onClick={() => decrement(id)}>
+                    -
+                    </span>
+
+                    <span className="btn btn-count mx-1" >{count}</span>
+
+                    <span className="btn btn-cyan mx-1" onClick={() => increment(id)}>
+                    +
+                    </span>
+
+                </div>
+            
+            </div>
+        </div>
+
+        <div className="col-10 mx-auto col-lg-2">
+            <div className="trash-icon" onClick={() => removeItem(id)}>
+                <i className="fas fa-trash" />
+            </div>          
+        </div>
+
+        <div className="col-10 mx-auto col-lg-2">
+            <strong>Item Total : Rs. {total}</strong>           
+        </div>
+
         </div>
     )
 }
--- src/components/Default.js
+++ src/components/Default.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react'
 
 export default class Default extends Component {
     render() {
+
+        console.log(this.props);
+
         return (
-            <div>
-                <h3>Page Not Found</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+                        <h1 className="display-3 text-danger">404</h1>
+                        <h2>Page Not Found</h2>
+
+                        <h3>The page <span className="text-danger">{this.props.location.pathname}</span>{""} you are tring to access was not found</h3>
+                    </div>
+                </div>
             </div>
         )
     }
